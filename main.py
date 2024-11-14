5aa52d3c1e2896ae58a6dc7b91674eea74013099
Logos106 <logos106@outlook.com>
2024-11-14 06:51:06 +0100

    Ignored Image.json, modified product numbers to insert

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import sys
 sys.path.append('src')
 
 from product import ProductUpdater
-from category import CategoryUpdater
 
 STORE_HASH = "5byitdbjtb"
 API_TOKEN = "t4iu0pxpzxck0h5azrwmy8u3w9994q2"
@@ -13,8 +12,8 @@ if __name__ == '__main__':
     productFile = "Products.json"
     productInfoFile = "ProductsInfo.json"
     imageFile = "Image.json"
-    # data = updater.insert_products_to_bigcommerce(productFile, productInfoFile)
+    data = updater.insert_products_to_bigcommerce(productFile, productInfoFile)
     # data = updater.create_variations_in_bigcommerce(productInfoFile)
-    data = updater.create_images_in_bigcommerce(productInfoFile, imageFile)
+    # data = updater.create_images_in_bigcommerce(productInfoFile, imageFile)
     
     
