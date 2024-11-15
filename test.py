c9127ac5a1ed8643934b02b93e0c5ad1690b86ee
Logos106 <logos106@outlook.com>
2024-11-15 18:24:54 +0100

    Product last

--- test.py
+++ test.py
@@ -74,7 +74,7 @@ def get_new_product_id(pinfo_file, image_file):
     with open(pinfo_file, 'r') as f:
         pinfo_data = json.load(f)
         
-        for product_info in pinfo_data[40000:50001]:
+        for product_info in pinfo_data[50000:60001]:
             old_product_id = product_info['id']
             product_sku = product_info['sku']
             
@@ -91,7 +91,7 @@ def get_new_product_id(pinfo_file, image_file):
             print(old_product_id, new_product_id)
 
 
-    file_path = f"ID_Mapping4.json"
+    file_path = f"ID_Mapping5.json"
     with open(file_path, 'w') as f:
         json.dump(id_mapping, f, indent=4)
 
