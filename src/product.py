ec79c3636c64ebc8b8d06a5497e436e0419a7bd6
Logos106 <logos106@outlook.com>
2024-11-18 18:26:22 +0100

    Changed a lot

--- src/product.py
+++ src/product.py
@@ -44,8 +44,8 @@ class ProductUpdater:
                 url = f"{self.bigbuy_base_url}products?keyword={product_sku}"
                 response = requests.get(url, headers=self.bigbuy_headers)
                 data = response.json()
-                product_weight = data['data']['weight']
-                product_price = data['data'] ['retail_price']
+                product_weight = data['data'][0]['weight']
+                product_price = data['data'][0]['retail_price']
                 print(product_sku, product_weight, product_price)
                 continue
 
